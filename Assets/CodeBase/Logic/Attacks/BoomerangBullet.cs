d9dca535946316feb27dcf5162fa4b08c19bc113
GeraDot4815 <lazurit216@gmail.com>
2024-10-04 20:03:56 +0300

    БУмеранг атака

---
+++ Assets/CodeBase/Logic/Attacks/BoomerangBullet.cs
@@ -0,0 +1,105 @@
+using CodeBase.Logic.Player;
+using DG.Tweening;
+using Unity.Burst.Intrinsics;
+using UnityEngine;
+namespace CodeBase.Logic.Attacks
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class BoomerangBullet : MonoBehaviour
+    {
+        [HideInInspector] public int damage;
+        [HideInInspector] public Attack parentAttack;
+
+        public float speed;
+        [SerializeField] private float speedCoof = 3;
+
+        public CircleCollider2D _collider2D;
+
+        [SerializeField] private Transform visual;
+
+        [HideInInspector] public Vector3 targetPos = Vector3.zero;
+        private bool isTargetPos = false;
+        private float minDist = 0.1f;
+
+        private Rigidbody2D rb;
+        private GameObject player;
+
+        private bool canAttack = true;
+        [SerializeField] private float attackDelay = 0.1f;
+        private float timeBtwAttack;
+
+        [SerializeField] private int lifeTime = 10;
+        private void Start()
+        {
+            player = FindFirstObjectByType<PlayerController>().gameObject;
+
+            timeBtwAttack = attackDelay;
+
+            rb = GetComponent<Rigidbody2D>();
+            rb.gravityScale = 0;
+            rb.freezeRotation = true;
+            visual.DORotate(new Vector3(0, 0, 360), 0.1f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetRelative();
+            OnAttack();
+
+            Invoke("NadoelKrutitsa", lifeTime);
+        }
+        public void OnAttack()
+        {
+            rb.linearVelocity = transform.right * speed;
+        }
+        private void Update()
+        {
+            if (!isTargetPos)
+            {
+                if (Mathf.Abs(Vector2.Distance(transform.position, targetPos)) < minDist)
+                {
+                    isTargetPos = true;
+                }
+            }
+            if (isTargetPos)
+            {
+                rb.linearVelocity = Vector2.zero;
+                transform.position = Vector2.Lerp(transform.position, player.transform.position, speed * speedCoof * Time.deltaTime);
+                if (Mathf.Abs(Vector2.Distance(transform.position, player.transform.position)) < minDist)
+                {
+                    NadoelKrutitsa();
+                }
+            }
+            if (!canAttack) timeBtwAttack -= Time.deltaTime;
+            if (timeBtwAttack < 0)
+            {
+                timeBtwAttack = attackDelay;
+                canAttack=true;
+            }
+
+        }
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (canAttack) DealDamage();
+        }
+        private void DealDamage()
+        {
+
+            Health health;
+            foreach (var col in Physics2D.OverlapCircleAll(_collider2D.bounds.center,
+                _collider2D.radius))
+            {
+                if (col.TryGetComponent(out health))
+                {
+                    if (health.Team != parentAttack.Team)
+                    {
+                        health.TakeDamage(damage);
+                        Debug.Log("Êטייא!");
+                        canAttack = false;
+                    }
+                }
+            }
+        }
+        private void NadoelKrutitsa()
+        {
+            visual.DOKill();
+            Destroy(gameObject);
+        }
+    }
+    
+}
