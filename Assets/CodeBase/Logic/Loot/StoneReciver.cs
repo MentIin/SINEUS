3d60722f4c33c2335fb5222c132f2291a961be35
MentIin <81743613+MentIin@users.noreply.github.com>
2024-10-05 23:25:59 +0300

    Reciver

---
+++ Assets/CodeBase/Logic/Loot/StoneReciver.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CodeBase.Infrastructure.Data.PlayerData;
+using CodeBase.Infrastructure.Services;
+using CodeBase.Infrastructure.Services.PersistentProgress;
+using CodeBase.Infrastructure.StaticData;
+using UnityEngine;
+
+namespace CodeBase.Logic.Loot
+{
+    public class StoneReciver : MonoBehaviour
+    {
+        public StoneStaticData Data;
+        public SpriteRenderer renderer;
+
+        private GameData _progressGameData;
+
+        private void Start()
+        {
+            renderer.sprite = Data.Icon;
+            _progressGameData = AllServices.Container.Single<PersistentProgressService>().Progress.GameData;
+            
+            _progressGameData.MagicStoneChanged += MagicStoneChanged;
+            MagicStoneChanged();
+        }
+
+        private void MagicStoneChanged()
+        {
+            if (_progressGameData.Recived.Contains(Data.Type))
+            {
+                renderer.color = Color.white;
+            }
+            else
+            {
+                renderer.color = Color.black;
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _progressGameData.MagicStoneChanged -= MagicStoneChanged;
+        }
+
+        public void Recive()
+        {
+            _progressGameData.Recive(Data.Type);
+        }
+    }
+}
