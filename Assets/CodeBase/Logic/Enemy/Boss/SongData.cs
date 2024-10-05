6d48de0430434f7e57512925648a7424c426ef58
MentIin <81743613+MentIin@users.noreply.github.com>
2024-10-06 00:23:54 +0300

    Boss + State machine is service

---
+++ Assets/CodeBase/Logic/Enemy/Boss/SongData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.Logic.Enemy.Boss
+{
+    [Serializable]
+    public class SongData
+    {
+        [Header("Duration in sec")]
+        public float Duration = 130f;
+
+        public List<float> Timings = new List<float>();
+    }
+}
