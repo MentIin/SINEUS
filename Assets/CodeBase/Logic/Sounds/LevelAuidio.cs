a784949d47c1e27c97d580f6b815e58955f5a8ea
MentIin <81743613+MentIin@users.noreply.github.com>
2024-10-05 23:34:26 +0300

    LevelAuidio

---
+++ Assets/CodeBase/Logic/Sounds/LevelAuidio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CodeBase.Infrastructure.Data.PlayerData;
+using CodeBase.Infrastructure.Services;
+using CodeBase.Infrastructure.Services.Audio;
+using CodeBase.Infrastructure.Services.PersistentProgress;
+using UnityEngine;
+
+namespace CodeBase.Logic.Sounds
+{
+    public class LevelAuidio : MonoBehaviour
+    {
+        public GameData.MagicStonesTypes Type;
+        public AudioClip EvilClip;
+        public AudioClip GoodClip;
+        
+        
+        private AudioService audioService;
+        private GameData gameData;
+
+
+        private bool _good = false;
+
+        private void Start()
+        {
+            audioService = AllServices.Container.Single<AudioService>();
+            gameData = AllServices.Container.Single<PersistentProgressService>().Progress.GameData;
+
+
+            audioService.SetAmbient(EvilClip);
+            SetAudio();
+            gameData.MagicStoneChanged += SetAudio;
+        }
+
+        private void SetAudio()
+        {
+            if (gameData.Recived.Contains(Type) && !_good)
+            {
+                _good = true;
+                audioService.SetAmbient(GoodClip);
+            }
+            else if (_good)
+            {
+                audioService.SetAmbient(EvilClip);
+            }
+        }
+    }
+}
