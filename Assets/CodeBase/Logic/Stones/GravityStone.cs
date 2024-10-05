3658c455b95a34d106aa095995e2b9ce591dc953
MentIin <81743613+MentIin@users.noreply.github.com>
2024-10-05 09:09:01 +0300

    new stone types

---
+++ Assets/CodeBase/Logic/Stones/GravityStone.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeBase.Logic.Stones
+{
+    class GravityStone : BaseStone
+    {
+        public CharacterController2D CharacterController;
+        protected override void Deactivate()
+        {
+            CharacterController.SetGravityScale(1f);
+        }
+
+        protected override void Activate()
+        {
+            CharacterController.SetGravityScale(-1f);
+        }
+    }
+}
